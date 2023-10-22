42f211c9ee7d2f7e95582c4eb4dfe18b210a962d
cmwema <calwbmwema65@gmail.com>
2023-10-22 18:47:54 +0300

    added menu section

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -1,9 +1,23 @@
-import React from "react";
 import HeroSection from "./HeroSection";
+import React, { useEffect, useRef, useState } from "react";
+import { motion } from "framer-motion";
+import { MdChevronLeft, MdChevronRight } from "react-icons/md";
+import RowContainer from "./RowContainer";
+import MenuContainer from "./MenuContainer";
+import { useStateValue } from "../context/StateProvider";
 
 function MainContainer() {
+  const [{ foodItems, cartShow }, dispatch] = useStateValue();
+  const [scrollValue, setScrollValue] = useState(0);
+
+  useEffect(() => {}, [scrollValue, cartShow]);
   return (
-    <HeroSection />
+    <>
+      <HeroSection />
+      
+
+      <MenuContainer />
+    </>
   );
 }
 
